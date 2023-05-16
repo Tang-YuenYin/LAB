a0adb55000e1d5b17a8dbc038858fe576f412935
Tang-YuenYin <121240154+Tang-YuenYin@users.noreply.github.com>
2023-05-16 23:16:44 +0800

    Add files via upload

---
+++ Week9/Lab/Graph.java
@@ -0,0 +1,36 @@
+
+package Graph;
+public class Graph<T extends Comparable<T>, N extends Comparable<N>> extends WeightedGraph<T, N> {
+    
+    // Methods
+    public boolean addEdge(T source, T destination) {
+        if (head == null) return false;
+        if (!super.hasVertex(source) || !super.hasVertex(destination)) return false;
+        Vertex<T, N> sourceVertex = head;
+        while (sourceVertex != null) {
+            if (sourceVertex.vertexInfo.compareTo(source) == 0) {
+                // Reached source vertex, look for destination now
+                Vertex<T, N> destinationVertex = head;
+                while (destinationVertex != null) {
+                    if (destinationVertex.vertexInfo.compareTo(destination) == 0) {
+                        // Reached destination vertex, add edge here
+                        Edge<T, N> currentEdge = sourceVertex.firstEdge;
+                        Edge<T, N> newEdge = new Edge<>(destinationVertex, null, currentEdge);
+                        sourceVertex.firstEdge = newEdge;
+                        sourceVertex.outdeg++;
+                        destinationVertex.indeg++;
+                        return true;
+                    }
+                    destinationVertex = destinationVertex.nextVertex;
+                }
+            }
+            sourceVertex = sourceVertex.nextVertex;
+        }
+        return false;
+    }
+    
+        public boolean addUndirectedEdge(T source, T destination) {
+        return addEdge(source, destination) && addEdge(destination, source);
+    }
+       
+}
